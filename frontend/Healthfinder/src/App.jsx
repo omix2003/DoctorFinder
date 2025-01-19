d0ec8b24ed36f9f3b62290fa60e94e8d3c4b945b
Omkar Binage <omkarbinage0@gmail.com>
2025-01-19 18:12:50 +0530

    try 2

--- frontend/Healthfinder/src/App.jsx
+++ frontend/Healthfinder/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import HealthcarePage from './components/HealthcarePage';
+import axios from 'axios';
+
 
 export default function App() {
   return (
@@ -9,5 +11,31 @@ export default function App() {
         <HealthcarePage />
       </div>
     </Router>
+  );
+}
+
+function HealthcarePage() {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    // Make an API request to your backend
+    axios.get('http://localhost:5000/api/doctors') // Adjust the URL to your backend
+      .then(response => {
+        setDoctors(response.data); // Assuming the backend returns the list of doctors
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Doctors List</h1>
+      <ul>
+        {doctors.map(doctor => (
+          <li key={doctor.id}>{doctor.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
